1c0db71ce763c2ae3f2c285aeab1e41db83b92e4
AliSherTR <94902748+AliSherTR@users.noreply.github.com>
2024-08-27 15:40:23 +0500

    refactor: register user functionality

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,9 @@
 "use server";
 import * as z from "zod";
-
+import bcrypt from "bcryptjs";
 import { RegisterSchema } from "@/schemas";
 import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validated = RegisterSchema.safeParse(values);
@@ -13,11 +14,15 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 
     const { name, email, password } = validated.data;
 
+    const existingUser = await getUserByEmail(email);
+    if (existingUser) return { error: "User already exists" };
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     await db.user.create({
         data: {
             name,
             email,
-            password,
+            password: hashedPassword,
         },
     });
 
