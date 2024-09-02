17aaed40d749f9f47c0bcb80451668158d5fd738
AliSherTR <94902748+AliSherTR@users.noreply.github.com>
2024-09-02 19:38:28 +0500

    added todos for next tasks

--- app/invoices/page.tsx
+++ app/invoices/page.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { auth } from "@/auth";
 export default async function page() {
+    // TODO : Add invoice components and fetch specific invoices for users
+    // for the time being only add some dummy invoices to the database/
+    // this will invlove the concept of seeding the database so we will do that
     return <div> Invoices</div>;
 }
