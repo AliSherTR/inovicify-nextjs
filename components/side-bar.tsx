0621b785d2b7582cda861410d9c87af3d8c34d20
AliSherTR <94902748+AliSherTR@users.noreply.github.com>
2024-09-04 20:19:56 +0500

    fixed: hydration warning in sidebar

--- components/side-bar.tsx
+++ components/side-bar.tsx
@@ -4,16 +4,17 @@ import { useTheme } from "next-themes";
 import { Moon, Sun } from "lucide-react";
 import Logo from "@/public/logo.svg";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function SideBar() {
     const { setTheme, theme } = useTheme();
-    function changeTheme(theme: string | undefined) {
-        if (theme === "light") {
-            setTheme("dark");
-        } else {
-            setTheme("light");
-        }
-    }
+
+    const [isClient, setIsClient] = useState(false);
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    if (!isClient) return;
     return (
         <div className=" flex xl:flex-col items-center justify-between  h-full bg-[#252945]  rounded-r-3xl">
             <div className=" xl:w-full w-[10%] h-[7rem]  flex items-center ">
@@ -29,13 +30,28 @@ export default function SideBar() {
                     <div className=" absolute inset-0 top-14 z-10 bg-[#9277ff]/90 rounded-tl-2xl group-hover:top-3 transition-all ease-linear duration-300 "></div>
                 </div>
             </div>
-            <div className="p-6 xl:self-end self-center xl:w-full flex items-center xl:justify-center justify-end">
-                <button
-                    className=" text-white"
-                    onClick={() => changeTheme(theme)}
-                >
-                    {theme === "light" ? <Moon /> : <Sun />}
-                </button>
+            <div
+                className="p-6 xl:self-end self-center xl:w-full flex items-center xl:justify-center justify-end"
+                suppressHydrationWarning
+            >
+                {theme === "light" && (
+                    <button
+                        className=" text-white"
+                        onClick={() => setTheme("dark")}
+                        suppressHydrationWarning
+                    >
+                        <Moon />
+                    </button>
+                )}
+                {theme === "dark" && (
+                    <button
+                        className=" text-white"
+                        onClick={() => setTheme("light")}
+                        suppressHydrationWarning
+                    >
+                        <Sun />
+                    </button>
+                )}
             </div>
         </div>
     );
